90d9e9f6e9a72f39da77afdbe7abee347980f689
salsazly <kanawut246@gmail.com>
2025-12-04 15:35:01 +0700

    step2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+    $this->call([
+        UserSeeder::class,
+        StoreSeeder::class,
+        ProductCategorySeeder::class,
+        ProductSeeder::class,
+    ]);
+
         // User::factory(10)->create();
 
         User::factory()->create([
